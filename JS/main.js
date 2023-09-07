c283a2ba1c15174fe13195ddcde8fdeeae803c89
Martín Maguna <martinmaguna33@gmail.com>
2023-09-06 23:26:22 -0300

    primer commit

---
+++ JS/main.js
@@ -0,0 +1,63 @@
+//Variables
+
+//Variable String
+
+const PARTIDO_POLITICO= "Frente Amplio";
+
+let votosInterna= "Partido Comunista";
+votosInterna="Partido Socialista";
+
+//Variables Number
+let dataInterna = 33
+
+
+const DIPUTADOS= 30; 
+console.log("Diputados:");
+console.log (DIPUTADOS);
+
+const SENADORES= 10;
+console.log("Senadores:");
+console.log(SENADORES)
+
+const REPRESENTANTES= DIPUTADOS + SENADORES;
+
+const DIRECTORES= 5;
+
+console.log("Representantes FA:");
+console.log(REPRESENTANTES);
+console.log("FA en el gobierno:")
+console.log(REPRESENTANTES + DIRECTORES)
+
+console.log(PARTIDO_POLITICO);
+console.log(votosInterna);
+console.log("Votación 2024");
+
+let palabraUno="Coder";
+let palabraDos="House";
+
+let resultado= palabraUno + " " + palabraDos;
+console.log(resultado);
+
+let nombreIngresado = prompt ("Ingrese su nombre");
+console.log (nombreIngresado);
+
+let saludo= alert ("Hola")
+console.log ("Hola" + nombreIngresado);
+
+//let concat 
+
+//true and false
+//Booleans
+//Negative values
+
+//===
+//!==
+
+//Arrays
+
+//Operadores
+//Equality operator
+//Inequality operator
+
+//prompt
+//alert
